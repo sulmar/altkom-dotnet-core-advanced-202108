3120980e5439a5956de6d9b1346fe2dee872af0c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-08-30 10:49:21 +0200

    Dodanie interfejsu

---
+++ Altkom.Shop.IServices/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using Altkom.Shop.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Altkom.Shop.IServices
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> Get();
+        Customer Get(int id);
+        void Add(Customer customer);
+        void Update(Customer customer);
+        void Remove(int id);
+
+    }
+}
