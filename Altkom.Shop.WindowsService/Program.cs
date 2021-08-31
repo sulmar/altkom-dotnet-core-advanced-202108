8047b95c6f62dfece18acdc212160e29025045dc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-08-31 16:49:50 +0200

    Windows Service

---
+++ Altkom.Shop.WindowsService/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using Topshelf;
+
+namespace Altkom.Shop.WindowsService
+{
+    class Program
+    {
+        // dotnet add package Topshelf
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HostFactory.Run(p =>
+            {
+                p.Service<LoggerService>();
+                p.SetDisplayName("Altkom Service");
+                p.SetDescription("Opis usługi");
+                p.StartAutomatically();
+            });
+
+            
+        }
+    }
+
+    public class LoggerService : ServiceControl
+    {
+        private Timer timer;
+
+        public LoggerService()
+        {
+            timer = new Timer();
+            
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Log("Started!");
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Log("Stopped.");
+
+            return true;
+        }
+
+        private void Log(string message)
+        {
+            File.AppendAllText(@"c:\temp\log.txt", message);
+        }
+    }
+}
