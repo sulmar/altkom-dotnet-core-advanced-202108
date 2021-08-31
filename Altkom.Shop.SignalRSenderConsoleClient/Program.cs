90606e42f2064b0cfd110e47790c5bdcb93db09d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-08-31 09:42:42 +0200

    SignalR

---
+++ Altkom.Shop.SignalRSenderConsoleClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using Altkom.Shop.Fakers;
+using Altkom.Shop.Models;
+using Bogus;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Altkom.Shop.SignalRSenderConsoleClient
+{
+    class Program
+    {
+
+        // =< C# 7.0
+        //static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+
+        //static async Task MainAsync(string[] args)
+        //{
+
+        //}
+
+        static async Task Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Hello Signal-R Sender!");
+
+            const string url = "http://localhost:5000/signalr/customers";
+
+            // dotnet add package Microsoft.AspNetCore.SignalR.Client
+
+            HubConnection connection = new HubConnectionBuilder()
+                .WithUrl(url)
+                .Build();
+
+            Console.WriteLine("Connecting...");
+
+            await connection.StartAsync();
+            
+            Console.WriteLine($"Connected {connection.ConnectionId}.");
+
+            Faker<Customer> customerFaker = new CustomerFaker(new AddressFaker());
+
+            Customer customer = customerFaker.Generate();
+
+            await connection.SendAsync("SendCustomer", customer);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            Console.ResetColor();
+
+        }
+    }
+}
---
+++ Altkom.Shop.SignalRServer/Hubs/CustomersHub.cs
@@ -0,0 +1,30 @@
+﻿using Altkom.Shop.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altkom.Shop.SignalRServer.Hubs
+{
+    public class CustomersHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Trace.WriteLine($"Connected {this.Context.ConnectionId}");
+
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendCustomer(Customer customer)
+        {
+            await Clients.Others.SendAsync("YouHaveGotNewCustomer", customer);
+        }
+
+        public async Task Ping(string message)
+        {
+            await Clients.Caller.SendAsync("pong", message);
+        }
+    }
+}
